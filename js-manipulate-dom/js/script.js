b5b2472586c850d71dcbee586c327e6901d9ce01
Caroline Barbosa <bo.varig@gmail.com>
2022-09-05 21:01:54 -0300

    created the function to update the statistics section when the buttons in the equipment section are clicked

--- js-manipulate-dom/js/script.js
+++ js-manipulate-dom/js/script.js
@@ -1,5 +1,8 @@
-//busca todos os elementos que possuem o data-attribute
+//busca todos os elementos que possuem o data-attribute data-control para adicionar um evento
 const control = document.querySelectorAll("[data-control]");
+//busca todos os elementos que possuem o data-attribute data-statistics para que os elementos sejam atualizados no HTML quando clicados
+const statistics = document.querySelectorAll("[data-statistics]")
+// objetos que contém as propriedades dos elementos de estatísticas
 const pecas = {
   "arms": {
       "strength": 29,
@@ -34,10 +37,12 @@ const pecas = {
   }
 };
 
-//cada vez que o elemento for clicado, a função irá retornar o data-attribute e seu elemento pai
+//cada vez que o elemento for clicado, a função irá retornar o data-attribute e seu elemento pai e adicionará o evento
 control.forEach((element) => {
   element.addEventListener('click', (e) => {
     manipulatesData(e.target.dataset.control, e.target.parentNode);
+    //chamar a função
+    updateStatistics(e.target.dataset.peca);
   });
 });
 
@@ -52,4 +57,19 @@ function manipulatesData(operation, control) {
     peca.value = parseInt(peca.value) + 1;
   };
 };
+
+function updateStatistics(peca) {
+  //esse valor será a referência de incremento
+  //console.log(pecas[peca]);
+  //atualizar o texto de cada elemento quando o botão for clicado
+  statistics.forEach((element) => {
+    //esse valor será refenrência no incremento
+    //console.log(element.dataset.statistics);
+    // ao clicar em cada elemento queremos atualizar o texto(textContent) 
+    //mas quando for incrementado/decrementado ele deve ter o valor somado/subtraído
+    //vamos transformar o texto em number e o valor atual será somado ao incremento do botão
+    element.textContent = parseInt(element.textContent) + pecas[peca][element.dataset.statistics]
+  });
+};
+
 
